79e35b06bc62d11a4d4b74ab173036162071d8ae
hkr hasan <learnwithhkr@gmail.com>
2020-08-08 07:35:46 +0530

    initial commit

---
+++ backend/build/precache-manifest.ea36176252de387116aaa25511cc23e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b73506e2cab848b5835a59039266ef9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5126af94189d940b18af",
+    "url": "/static/js/2.ca97b1b3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ca97b1b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1bc317aaa859b0ede539",
+    "url": "/static/js/main.6dabc36e.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  }
+]);
